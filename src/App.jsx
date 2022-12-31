a3212b44db18e8b1adcadd517f4f39234d389a73
kajal <kajalneha.nk@gmail.com>
2023-01-01 01:23:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import SlotM from './slotMachine';
+
+const App = () =>{
+    return (
+        <>
+    <h1 className='heading_style'>
+        🎰 Welcome to {" "}
+        <span style={{fontWeight: "bold"}}>Slot machine game</span> 🎰 
+        
+    </h1>
+    <div className='slotmachine'>
+    <SlotM x=' 😄' y= ' 😄' z=' 😄' />
+    <SlotM x=' 😏' y= ' 😃' z=' 😠' />
+    <SlotM x=' 🍎' y= ' 🍌' z=' 🍎'/>
+    <SlotM x=' 👪' y= ' 👪' z=' 👪'/>
+
+    </div>
+    </>
+    );
+};
+export default App;
